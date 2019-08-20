726d12bbe676f807c9eccc04010d943cfd67dc89
Luis Galeas <luis.galeasjr@gmail.com>
2019-08-20 08:56:38 -0400

    added tic-tac-toe game to repo

---
+++ src/Components/Square.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Square = (props) => {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
+}
+
+class Board extends React.Component {
+    renderSquare(i) {
+        return (
+            <Square
+                value={this.props.squares[i]}
+                onClick={() => this.props.onClick(i)}
+            />
+        );
+    }
+}
+
+export default Square
